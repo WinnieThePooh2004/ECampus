d578023340ac33b8792619fd166b48991c19e9d5
WinnieThePooh2004 <vova2004hunko@icloud.com>
2022-12-21 15:52:35 +0200

    merged IRelationRepository and IRelationshipsRepository into one interface

--- UniversityTimetable/src/UniversityTimetable.Shared/Interfaces/Repositories/IRelationshipsRepository.cs
+++ UniversityTimetable/src/UniversityTimetable.Shared/Interfaces/Repositories/IRelationshipsRepository.cs
@@ -1,31 +1,17 @@
-﻿using System.Linq.Expressions;
-using UniversityTimetable.Shared.Interfaces.Data;
+﻿using UniversityTimetable.Shared.Interfaces.Data;
 using UniversityTimetable.Shared.Interfaces.ModelsRelationships;
 
-namespace UniversityTimetable.Shared.Interfaces.Repositories
+namespace UniversityTimetable.Shared.Interfaces.Repositories;
+
+public interface IRelationshipsRepository<in TLeftTable, TRightTable, TRelations>
+    where TLeftTable : class, IModel, IModelWithManyToManyRelations<TRightTable, TRelations>, new()
+    where TRightTable : class, IModel, new()
+    where TRelations : class, IRelationModel<TLeftTable, TRightTable>, new()
 {
-    /// <summary>
-    /// objects implementing this interface should not invoke DbContext.SaveChanges(Async) 
-    /// it only helps to create/update relation models between TLeftTable and TRightTable objects 
-    /// if you need to save some changes, do it in main repository
-    /// </summary>
-    public interface IRelationshipsRepository<in TLeftTable, TRightTable, TRelations>
-        where TLeftTable : class, IModel, IModelWithManyToManyRelations<TRightTable, TRelations>, new()
-        where TRightTable : class, IModel, new()
-        where TRelations : class, IRelationModel<TLeftTable, TRightTable>, new()
-    {
-        /// <summary>
-        /// this method replaces all navigation properties of type TLeftTable ot TRelations, 
-        /// if you need to use objects from related, save it before using this method
-        /// </summary>
-        /// <param name="model"></param>
-        void CreateRelationModels(TLeftTable model);
-        /// <summary>
-        /// this method will call database to find required relation models, if your model already has relation models, use UpdateLoadedRelations 
-        /// </summary>
-        /// <param name="model"></param>
-        /// <returns></returns>
-        Task UpdateRelations(TLeftTable model);
-        Task UpdateLoadedRelations(TLeftTable model);
-    }
+    void CreateRelationModels(TLeftTable model);
+    Task UpdateRelations(TLeftTable model);
+    void UpdateLoadedRelations(TLeftTable model);
+    Task<TRelations> CreateRelation(int leftTableId, int rightTableId);
+    Task<TRelations> DeleteRelation(int leftTableId, int rightTableId);
+
 }
