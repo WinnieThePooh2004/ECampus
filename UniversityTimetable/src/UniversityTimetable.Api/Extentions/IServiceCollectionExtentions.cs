56d5e0c3cd210936f35a2f3ebe7d794a4903033c
WinnieThePooh2004 <vova2004hunko@icloud.com>
2022-12-21 15:53:22 +0200

    updated IServiceCollectionExtensions.cs

--- UniversityTimetable/src/UniversityTimetable.Api/Extentions/IServiceCollectionExtentions.cs
+++ UniversityTimetable/src/UniversityTimetable.Api/Extentions/IServiceCollectionExtentions.cs
@@ -5,27 +5,25 @@ using UniversityTimetable.Shared.Interfaces.Repositories;
 using UniversityTimetable.Shared.Interfaces.Services;
 using UniversityTimetable.Shared.QueryParameters;
 
-namespace UniversityTimetable.Api.Extentions
-{
-    public static class IServiceCollectionExtentions
-    {        
-        public static IServiceCollection AddDataSelector<TModel, TParameters, TImplementation>(this IServiceCollection services)
-            where TModel : class, IModel
-            where TParameters : IQueryParameters<TModel>
-            where TImplementation : class, IDataSelector<TModel, TParameters>
-            => services.AddScoped<IDataSelector<TModel, TParameters>, TImplementation>();
+namespace UniversityTimetable.Api.Extentions;
 
-        public static IServiceCollection AddDefaultServices<TModel, TDTO, TParameters>(this IServiceCollection services)
-            where TParameters : class, IQueryParameters<TModel>, new()
-            where TModel : class, IModel, new()
-            where TDTO : class, IDataTransferObject, new()
-        {
-            services.AddScoped<IService<TDTO, TParameters>, Service<TDTO, TParameters, TModel>>();
-            services.AddScoped<IBaseService<TDTO>, BaseService<TDTO, TModel>>();
-            services.AddScoped<IRepository<TModel, TParameters>, Repository<TModel, TParameters>>();
-            services.AddScoped<IBaseRepository<TModel>, BaseRepository<TModel>>();
-            return services;
-        }
-            
+public static class IServiceCollectionExtensions
+{        
+    public static IServiceCollection AddDataSelector<TModel, TParameters, TImplementation>(this IServiceCollection services)
+        where TModel : class, IModel
+        where TParameters : IQueryParameters<TModel>
+        where TImplementation : class, IDataSelector<TModel, TParameters>
+        => services.AddScoped<IDataSelector<TModel, TParameters>, TImplementation>();
+
+    public static IServiceCollection AddDefaultServices<TModel, TDto, TParameters>(this IServiceCollection services)
+        where TParameters : class, IQueryParameters<TModel>, new()
+        where TModel : class, IModel, new()
+        where TDto : class, IDataTransferObject, new()
+    {
+        services.AddScoped<IBaseRepository<TModel>, BaseRepository<TModel>>();
+        services.AddScoped<IService<TDto, TParameters>, Service<TDto, TParameters, TModel>>();
+        services.AddScoped<IBaseService<TDto>, BaseService<TDto, TModel>>();
+        services.AddScoped<IRepository<TModel, TParameters>, Repository<TModel, TParameters>>();
+        return services;
     }
 }
