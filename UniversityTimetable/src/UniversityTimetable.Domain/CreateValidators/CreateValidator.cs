227ac4b938cf5083bcca4fa3efbfb348fdac1953
WinnieThePooh2004 <vova2004hunko@icloud.com>
2022-12-24 15:22:14 +0200

    added validator classes

---
+++ UniversityTimetable/src/UniversityTimetable.Domain/CreateValidators/CreateValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using UniversityTimetable.Shared.Interfaces.Data;
+
+namespace UniversityTimetable.Domain.CreateValidators;
+
+public class CreateValidator<TDto> : ICreateValidator<TDto>
+    where TDto : class, IDataTransferObject
+{
+    private readonly IValidator<TDto> _fluentValidationValidator;
+
+    public CreateValidator(IValidator<TDto> fluentValidationValidator)
+    {
+        _fluentValidationValidator = fluentValidationValidator;
+    }
+
+    public async Task<Dictionary<string, string>> ValidateAsync(TDto dataTransferObject) =>
+        new((await _fluentValidationValidator.ValidateAsync(dataTransferObject))
+            .Errors.Select(error => new KeyValuePair<string,string>(error.PropertyName, error.ErrorMessage)));
+
+}
---
+++ UniversityTimetable/src/UniversityTimetable.Domain/UpdateValidators/UpdateValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using UniversityTimetable.Shared.Interfaces.Data;
+
+namespace UniversityTimetable.Domain.UpdateValidators;
+
+public class UpdateValidator<TDto> : IUpdateValidator<TDto> 
+    where TDto : class, IDataTransferObject
+{
+    private readonly IValidator<TDto> _fluentValidationValidator;
+
+    public UpdateValidator(IValidator<TDto> fluentValidationValidator)
+    {
+        _fluentValidationValidator = fluentValidationValidator;
+    }
+
+    public async Task<Dictionary<string, string>> ValidateAsync(TDto dataTransferObject) =>
+        new((await _fluentValidationValidator.ValidateAsync(dataTransferObject))
+            .Errors.Select(error => new KeyValuePair<string,string>(error.PropertyName, error.ErrorMessage)));
+}
