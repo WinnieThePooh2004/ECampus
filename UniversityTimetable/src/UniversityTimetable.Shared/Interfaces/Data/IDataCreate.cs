02196b5be467c96062ba9713f197805003c2ba8c
WinnieThePooh2004 <vova2004hunko@icloud.com>
2022-12-24 15:16:29 +0200

    interfaces to Create, Update, Delete

---
+++ UniversityTimetable/src/UniversityTimetable.Shared/Interfaces/Data/IDataCreate.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UniversityTimetable.Shared.Interfaces.Data;
+
+public interface IDataCreate<TModel>
+    where TModel : class, IModel, new()
+{
+    Task<TModel> CreateAsync(TModel model, DbContext context);
+}
