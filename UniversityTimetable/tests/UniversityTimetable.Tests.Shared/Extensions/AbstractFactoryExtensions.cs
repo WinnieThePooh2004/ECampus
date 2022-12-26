70d375702c754c1a74308f687d4dde129644040b
WinnieThePooh2004 <vova2004hunko@icloud.com>
2022-12-26 15:30:11 +0200

    now IAbstractFactory can create many instances using 1 method

---
+++ UniversityTimetable/tests/UniversityTimetable.Tests.Shared/Extensions/AbstractFactoryExtensions.cs
@@ -0,0 +1,13 @@
+﻿using AutoFixture;
+using UniversityTimetable.Tests.Shared.DataFactories;
+
+namespace UniversityTimetable.Tests.Shared.Extensions;
+
+public static class AbstractFactoryExtensions
+{
+    public static List<T> CrateMany<T>(this IAbstractFactory<T> factory, Fixture fixture, int amount)
+    {
+        return Enumerable.Range(0, amount).Select(i =>factory.CreateModel(fixture)).ToList();
+    }
+    
+}
