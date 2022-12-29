c78c385e3307e08589a8e5313fc61a83c3f340f9
WinnieThePooh2004 <vova2004hunko@icloud.com>
2022-12-29 23:22:45 +0200

    now add '[propertyName]=' instead of nothing when property value is null

--- UniversityTimetable/src/UniversityTimetable.Shared/Extensions/QueryParametersExtentions.cs
+++ UniversityTimetable/src/UniversityTimetable.Shared/Extensions/QueryParametersExtentions.cs
@@ -7,7 +7,7 @@ public static class QueryParametersExtensions
     public static string ToQueryString<TParams>(this TParams parameters)
     {
         var properties = parameters?.GetType().GetProperties()
-            .Select(p => $"{p.Name}={HttpUtility.UrlEncode(p.GetValue(parameters, null)?.ToString() ?? " ")}")
+            .Select(p => $"{p.Name}={HttpUtility.UrlEncode(p.GetValue(parameters, null)?.ToString())}")
             .ToList() ?? new List<string>();
         return string.Join("&", properties);
     }
