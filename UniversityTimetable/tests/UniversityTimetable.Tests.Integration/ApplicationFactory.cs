2be07934dbb09e074e4929b6d5e5bae99feba5a5
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-01-05 01:35:06 +0200

    added few integration tests

---
+++ UniversityTimetable/tests/UniversityTimetable.Tests.Integration/ApplicationFactory.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using UniversityTimetable.Infrastructure;
+using UniversityTimetable.Tests.Shared.TestDatabase;
+
+namespace UniversityTimetable.Tests.Integration;
+
+public class ApplicationFactory : WebApplicationFactory<Program>
+{
+    private readonly ApplicationDbContext _context;
+
+    public ApplicationFactory()
+    {
+        _context = TestDatabaseFactory.CreateContext();
+    }
+
+    public void InitDatabase()
+    {
+        _context.Database.EnsureCreated();
+        _context.SeedData();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(service =>
+                services.GetType() == typeof(DbContextOptions<ApplicationDbContext>));
+            if (descriptor is not null)
+            {
+                services.Remove(descriptor);
+            }
+            services.AddSingleton(_context);
+        });
+    }
+}
---
+++ UniversityTimetable/tests/UniversityTimetable.Tests.Integration/UnauthorizedResultTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using FluentAssertions;
+using UniversityTimetable.Shared.DataTransferObjects;
+
+namespace UniversityTimetable.Tests.Integration;
+
+public class UnauthorizedResultTests : IClassFixture<ApplicationFactory>
+{
+    private readonly HttpClient _client;
+    
+    public UnauthorizedResultTests(ApplicationFactory factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task PutToAuditories_ShouldReturn401_WhenUnauthorized()
+    {
+        var response = await _client.PutAsJsonAsync("api/Auditories", new AuditoryDto());
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
+    
+    [Fact]
+    public async Task PostToAuditories_ShouldReturn401_WhenUnauthorized()
+    {
+        var response = await _client.PostAsJsonAsync("api/Auditories", new AuditoryDto());
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task DeleteFromAuditories_ShouldReturn401_WhenUnauthorized()
+    {
+        var response = await _client.DeleteAsync($"api/Auditories/{100}");
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
+}
