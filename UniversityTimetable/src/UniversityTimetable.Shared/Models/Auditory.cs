6a614c66220227a55f5bdad091d905bb7d233111
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-01-13 00:29:56 +0200

    removed braces

--- UniversityTimetable/src/UniversityTimetable.Shared/Models/Auditory.cs
+++ UniversityTimetable/src/UniversityTimetable.Shared/Models/Auditory.cs
@@ -1,17 +1,16 @@
 ﻿using UniversityTimetable.Shared.Interfaces.Data.Models;
 using UniversityTimetable.Shared.Models.RelationModels;
 
-namespace UniversityTimetable.Shared.Models
-{
-    public class Auditory : IIsDeleted, IModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Building { get; set; } = string.Empty;
+namespace UniversityTimetable.Shared.Models;
 
-        public List<Class>? Classes { get; set; }
-        public bool IsDeleted { get; set; }
-        public List<User>? Users { get; set; }
-        public List<UserAuditory>? UsersIds { get; set; }
-    }
+public class Auditory : IIsDeleted, IModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Building { get; set; } = string.Empty;
+
+    public List<Class>? Classes { get; set; }
+    public bool IsDeleted { get; set; }
+    public List<User>? Users { get; set; }
+    public List<UserAuditory>? UsersIds { get; set; }
 }
