3c0d0569ba35e5409fbe90d63606dfafc058a6bc
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-01-13 00:34:22 +0200

    moved PagesInLine constant to DataPageSelect class

--- UniversityTimetable/src/UniversityTimetable.FrontEnd/Components/PageOptions/DataPageSelect.razor.cs
+++ UniversityTimetable/src/UniversityTimetable.FrontEnd/Components/PageOptions/DataPageSelect.razor.cs
@@ -1,16 +1,16 @@
 ﻿using Microsoft.AspNetCore.Components;
 
-namespace UniversityTimetable.FrontEnd.Components.PageOptions
-{
-    public partial class DataPageSelect
-    {
-        [Parameter] required public int MaxPageNumber { get; set; }
-        [Parameter] required public EventCallback<int> OnPageNumberChanged { get; set; }
-        private int _pageNumber = 1;
+namespace UniversityTimetable.FrontEnd.Components.PageOptions;
 
-        private Task PageNumberChanged(int pageNumber)
-        {
-            return OnPageNumberChanged.InvokeAsync(pageNumber);
-        }
+public partial class DataPageSelect
+{
+    private const int PagesInLine = 5;
+    [Parameter] required public int MaxPageNumber { get; set; }
+    [Parameter] required public EventCallback<int> OnPageNumberChanged { get; set; }
+    private int _pageNumber = 1;
+
+    private Task PageNumberChanged(int pageNumber)
+    {
+        return OnPageNumberChanged.InvokeAsync(pageNumber);
     }
 }
