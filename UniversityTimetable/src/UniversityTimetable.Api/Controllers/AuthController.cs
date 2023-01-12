5d4c2f9bba0b0725a7ef7bb15372e72ec2ff3efa
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-01-13 00:28:15 +0200

    removed Unused Logout Method

--- UniversityTimetable/src/UniversityTimetable.Api/Controllers/AuthController.cs
+++ UniversityTimetable/src/UniversityTimetable.Api/Controllers/AuthController.cs
@@ -4,34 +4,24 @@ using UniversityTimetable.Domain.Auth;
 using UniversityTimetable.Shared.DataTransferObjects;
 using IAuthorizationService = UniversityTimetable.Shared.Interfaces.Auth.IAuthorizationService;
 
-namespace UniversityTimetable.Api.Controllers
+namespace UniversityTimetable.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class AuthController : ControllerBase
+    private readonly IAuthorizationService _authorizationService;
+
+    public AuthController(IAuthorizationService authorizationService)
     {
-        private readonly IAuthorizationService _authorizationService;
+        _authorizationService = authorizationService;
+    }
 
-        public AuthController(IAuthorizationService authorizationService)
-        {
-            _authorizationService = authorizationService;
-        }
-
-        [HttpPost]
-        [AllowAnonymous]
-        [Route("login")]
-        public async Task<IActionResult> Login(LoginDto login)
-        {
-            return Ok(await _authorizationService.Login(login));
-        }
-
-        [HttpDelete]
-        [Route("logout")]
-        [Authorized]
-        public async Task<IActionResult> Logout()
-        {
-            await _authorizationService.Logout();
-            return NoContent();
-        }
+    [HttpPost]
+    [AllowAnonymous]
+    [Route("login")]
+    public async Task<IActionResult> Login(LoginDto login)
+    {
+        return Ok(await _authorizationService.Login(login));
     }
 }
