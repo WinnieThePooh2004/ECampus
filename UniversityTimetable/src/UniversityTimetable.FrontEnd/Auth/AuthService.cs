d49bce460ab0c970c8aea03e6c5771fee1d326d7
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-01-13 00:33:16 +0200

    now use jwt instead of cookies on served side

--- UniversityTimetable/src/UniversityTimetable.FrontEnd/Auth/AuthService.cs
+++ UniversityTimetable/src/UniversityTimetable.FrontEnd/Auth/AuthService.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using UniversityTimetable.FrontEnd.Requests.Interfaces;
+using UniversityTimetable.Shared.Exceptions.DomainExceptions;
 using UniversityTimetable.Shared.Extensions;
 
 namespace UniversityTimetable.FrontEnd.Auth;
@@ -17,9 +20,18 @@ public class AuthService : IAuthService
 
     public async Task Login(string email, string password, AuthenticationProperties properties = default!)
     {
+        if (_httpContextAccessor.HttpContext is null)
+        {
+            throw new HttpContextNotFoundExceptions();
+        }
         var login = new LoginDto { Email = email, Password = password };
         var user = await _requests.LoginAsync(login);
-        await _httpContextAccessor.HttpContext?.SignInAsync(user, properties)!;
+        var claims = HttpContextExtensions.CreateClaims(user);
+
+        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+        await _httpContextAccessor.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+            new ClaimsPrincipal(claimsIdentity), properties);
     }
 
 }
