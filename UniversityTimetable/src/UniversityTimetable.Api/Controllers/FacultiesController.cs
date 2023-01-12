e4360e547c87e87b2a64d305171795a1d4e46a53
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-01-13 00:36:43 +0200

    removed braces

--- UniversityTimetable/src/UniversityTimetable.Api/Controllers/FacultiesController.cs
+++ UniversityTimetable/src/UniversityTimetable.Api/Controllers/FacultiesController.cs
@@ -6,53 +6,52 @@ using UniversityTimetable.Shared.Enums;
 using UniversityTimetable.Shared.QueryParameters;
 using UniversityTimetable.Shared.Interfaces.Domain;
 
-namespace UniversityTimetable.Api.Controllers
+namespace UniversityTimetable.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FacultiesController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class FacultiesController : ControllerBase
+    private readonly IParametersService<FacultyDto, FacultyParameters> _service;
+    public FacultiesController(IParametersService<FacultyDto, FacultyParameters> service)
     {
-        private readonly IParametersService<FacultyDto, FacultyParameters> _service;
-        public FacultiesController(IParametersService<FacultyDto, FacultyParameters> service)
-        {
-            _service = service;
-        }
+        _service = service;
+    }
 
-        [HttpGet]
-        public async Task<IActionResult> Get([FromQuery] FacultyParameters parameters)
-        {
-            return Ok(await _service.GetByParametersAsync(parameters));
-        }
+    [HttpGet]
+    public async Task<IActionResult> Get([FromQuery] FacultyParameters parameters)
+    {
+        return Ok(await _service.GetByParametersAsync(parameters));
+    }
 
-        [HttpGet("{Id:int?}")]
-        [AllowAnonymous]
-        public async Task<IActionResult> Get(int? id)
-        {
-            return Ok(await _service.GetByIdAsync(id));
-        }
+    [HttpGet("{id:int?}")]
+    [AllowAnonymous]
+    public async Task<IActionResult> Get(int? id)
+    {
+        return Ok(await _service.GetByIdAsync(id));
+    }
 
-        [HttpPost]
-        [Authorized(UserRole.Admin)]
-        public async Task<IActionResult> Post([Bind("Name")] FacultyDto faculty)
-        {
-            await _service.CreateAsync(faculty);
-            return Ok(faculty);
-        }
+    [HttpPost]
+    [Authorized(UserRole.Admin)]
+    public async Task<IActionResult> Post([Bind("Name")] FacultyDto faculty)
+    {
+        await _service.CreateAsync(faculty);
+        return Ok(faculty);
+    }
 
-        [HttpPut]
-        [Authorized(UserRole.Admin)]
-        public async Task<IActionResult> Put(FacultyDto faculty)
-        {
-            await _service.UpdateAsync(faculty);
-            return Ok(faculty);
-        }
+    [HttpPut]
+    [Authorized(UserRole.Admin)]
+    public async Task<IActionResult> Put(FacultyDto faculty)
+    {
+        await _service.UpdateAsync(faculty);
+        return Ok(faculty);
+    }
 
-        [HttpDelete("{id:int?}")]
-        [Authorized(UserRole.Admin)]
-        public async Task<IActionResult> Delete(int? id)
-        {
-            await _service.DeleteAsync(id);
-            return Ok(id);
-        }
+    [HttpDelete("{id:int?}")]
+    [Authorized(UserRole.Admin)]
+    public async Task<IActionResult> Delete(int? id)
+    {
+        await _service.DeleteAsync(id);
+        return Ok(id);
     }
 }
