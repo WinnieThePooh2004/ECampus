8a941f658fdde64e197175ba4a29bf992782f322
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-01-16 23:04:58 +0200

    now OnChanged is EventCallback instead of Action in MultipleItemSelectTests, fixed bugs, changed MudSelect to custom InputSelect wrapper

---
+++ UniversityTimetable/src/UniversityTimetable.FrontEnd/Components/Input/Checkbox.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace UniversityTimetable.FrontEnd.Components.Input;
+
+public partial class Checkbox
+{
+    [Parameter]
+    public bool Value
+    {
+        get => _isChecked;
+        set
+        {
+            if (_isChecked == value)
+            {
+                return;
+            }
+
+            _isChecked = value;
+            ValueChanged.InvokeAsync(value);
+        }
+    }
+
+    [Parameter] public EventCallback<bool> ValueChanged { get; set; }
+    private bool _isChecked;
+}
