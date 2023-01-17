fcb9f4037d649383cf4e257afabe0f9ff008c0e9
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-01-17 22:32:09 +0200

    now use HttpContextAccessor instead of AuthProvider to get auth information

--- UniversityTimetable/src/UniversityTimetable.FrontEnd/Components/PageModels/DataTable.razor.cs
+++ UniversityTimetable/src/UniversityTimetable.FrontEnd/Components/PageModels/DataTable.razor.cs
@@ -4,8 +4,8 @@ using Microsoft.AspNetCore.Components.Authorization;
 using UniversityTimetable.Shared.Interfaces.Data;
 using UniversityTimetable.Shared.Interfaces.Data.Models;
 
-namespace UniversityTimetable.FrontEnd.Components.PageModels
-{
+namespace UniversityTimetable.FrontEnd.Components.PageModels;
+
     public partial class DataTable<TData, TParameters>
         where TData : class, IDataTransferObject
         where TParameters : class, IQueryParameters, new()
@@ -14,30 +14,28 @@ namespace UniversityTimetable.FrontEnd.Components.PageModels
         [Parameter] public bool ShowDeleteButton { get; set; } = true;
         [Parameter] public bool ShowEditButton { get; set; } = true;
 
-        [Parameter] public List<(string placeHolder, Expression<Func<TParameters, string?>>)> SearchTerms { get; set; } = new();
+    [Parameter] public List<(string placeHolder, Expression<Func<TParameters, string?>>)> SearchTerms { get; set; } = new();
         
-        /// <summary>
-        /// provide it without id
-        /// </summary>
-        [Parameter] public string EditLink { get; set; } = default!;
+    /// <summary>
+    /// provide it without id
+    /// </summary>
+    [Parameter] public string EditLink { get; set; } = default!;
 
-        [Parameter] public List<string> TableHeaders { get; set; } = new();
-        [Parameter] public List<Func<TData, string>> TableData { get; set; } = new();
+    [Parameter] public List<string> TableHeaders { get; set; } = new();
+    [Parameter] public List<Func<TData, string>> TableData { get; set; } = new();
 
-        [Parameter] public List<(string LinkName, Func<TData, string> LinkSource)> ActionLinks { get; set; } = new();
+    [Parameter] public List<(string LinkName, Func<TData, string> LinkSource)> ActionLinks { get; set; } = new();
 
-        [Parameter] public Action<TParameters> ParameterOptions { get; set; } = _ => { };
+    [Parameter] public Action<TParameters> ParameterOptions { get; set; } = _ => { };
 
-        [Inject] private AuthenticationStateProvider AuthProvider { get; set; } = default!;
-        private int TotalLinks => ActionLinks.Count + (ShowDeleteButton ? 1 : 0) + (ShowEditButton ? 1 : 0);
-        private bool _isAdmin;
+    [Inject] private IHttpContextAccessor HttpContextAccessor { get; set; } = default!;
+    private int TotalLinks => ActionLinks.Count + (ShowDeleteButton ? 1 : 0) + (ShowEditButton ? 1 : 0);
+    private bool _isAdmin;
 
-        protected override async Task OnInitializedAsync()
-        {
-            var authState = await AuthProvider.GetAuthenticationStateAsync();
-            _isAdmin = authState.User.IsInRole(nameof(UserRole.Admin));
-            ParameterOptions(Parameters);
-            await base.OnInitializedAsync();
-        }
+    protected override async Task OnInitializedAsync()
+    {
+        _isAdmin = HttpContextAccessor.HttpContext?.User.IsInRole(nameof(UserRole.Admin)) ?? false;
+        ParameterOptions(Parameters);
+        await base.OnInitializedAsync();
     }
 }
