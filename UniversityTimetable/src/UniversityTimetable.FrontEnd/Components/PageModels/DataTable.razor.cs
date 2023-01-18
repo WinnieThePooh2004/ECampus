101767ad2315fa0a4d0e25289c3aaf36b2b22c5c
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-01-18 15:12:03 +0200

    now pages dynamically change sort properties and sort order

--- UniversityTimetable/src/UniversityTimetable.FrontEnd/Components/PageModels/DataTable.razor.cs
+++ UniversityTimetable/src/UniversityTimetable.FrontEnd/Components/PageModels/DataTable.razor.cs
@@ -6,22 +6,24 @@ using UniversityTimetable.Shared.Interfaces.Data.Models;
 
 namespace UniversityTimetable.FrontEnd.Components.PageModels;
 
-    public partial class DataTable<TData, TParameters>
-        where TData : class, IDataTransferObject
-        where TParameters : class, IQueryParameters, new()
-    {
-        [Parameter] public string? CreateLink { get; set; }
-        [Parameter] public bool ShowDeleteButton { get; set; } = true;
-        [Parameter] public bool ShowEditButton { get; set; } = true;
+public partial class DataTable<TData, TParameters>
+    where TData : class, IDataTransferObject
+    where TParameters : class, IQueryParameters, new()
+{
+    [Parameter] public string? CreateLink { get; set; }
+    [Parameter] public bool ShowDeleteButton { get; set; } = true;
+    [Parameter] public bool ShowEditButton { get; set; } = true;
 
-    [Parameter] public List<(string placeHolder, Expression<Func<TParameters, string?>>)> SearchTerms { get; set; } = new();
-        
+    [Parameter]
+    public List<(string placeHolder, Expression<Func<TParameters, string?>>)> SearchTerms { get; set; } = new();
+
     /// <summary>
     /// provide it without id
     /// </summary>
-    [Parameter] public string EditLink { get; set; } = default!;
+    [Parameter]
+    public string EditLink { get; set; } = default!;
 
-    [Parameter] public List<string> TableHeaders { get; set; } = new();
+    [Parameter] public List<(string header, string propertyName)> TableHeaders { get; set; } = new();
     [Parameter] public List<Func<TData, string>> TableData { get; set; } = new();
 
     [Parameter] public List<(string LinkName, Func<TData, string> LinkSource)> ActionLinks { get; set; } = new();
@@ -37,5 +39,17 @@ namespace UniversityTimetable.FrontEnd.Components.PageModels;
         _isAdmin = HttpContextAccessor.HttpContext?.User.IsInRole(nameof(UserRole.Admin)) ?? false;
         ParameterOptions(Parameters);
         await base.OnInitializedAsync();
+    }
+
+    private Task OnPropertyNameClicked(string propertyName)
+    {
+        if (Parameters.OrderBy == propertyName)
+        {
+            Parameters.SortOrder = (SortOrder)((int)(Parameters.SortOrder + 1) % 2);
+            return RefreshData();
+        }
+
+        Parameters.OrderBy = propertyName;
+        return RefreshData();
     }
 }
