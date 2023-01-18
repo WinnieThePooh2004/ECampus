79df6e1fd5d445499dd70d6ebfd5053468b719ab
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-01-18 14:13:01 +0200

    removed obsolete comments

--- UniversityTimetable/src/UniversityTimetable.FrontEnd/Components/PageModels/TimetableModel.razor.cs
+++ UniversityTimetable/src/UniversityTimetable.FrontEnd/Components/PageModels/TimetableModel.razor.cs
@@ -6,14 +6,9 @@ namespace UniversityTimetable.FrontEnd.Components.PageModels
     public partial class TimetableModel
     {
         [Parameter] public List<Func<ClassDto, string>> DescriptionList { get; set; } = new();
-
-        /// <summary>
-        /// takes day of week and number of class as parameters
-        /// </summary>
+        
         [Parameter] public string CreateLink { get; set; } = string.Empty;
-        /// <summary>
-        /// provide it without id
-        /// </summary>
+        
         [Parameter] public string EditLink { get; set; }  = string.Empty;
 
         [Parameter] public Func<IClassRequests, Task<Timetable>> RefreshData { get; set; } = default!;
