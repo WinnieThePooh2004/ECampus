40a302232c6cb2d2cd7fea9ad1a9e8c574643175
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-01-21 00:21:04 +0200

    admins now can resister new users

---
+++ UniversityTimetable/src/UniversityTimetable.FrontEnd/Pages/AdminPages/CreateNewUser.razor.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Components;
+using UniversityTimetable.FrontEnd.Requests;
+using UniversityTimetable.FrontEnd.Validation.Interfaces;
+
+namespace UniversityTimetable.FrontEnd.Pages.AdminPages;
+
+public partial class CreateNewUser
+{
+    [Inject] private IUserRolesRequests UserRolesRequests { get; set; } = default!;
+    [Inject] private IUserValidatorFactory UserValidatorFactory { get; set; } = default!;
+
+    [Inject] private NavigationManager NavigationManager { get; set; } = default!;
+    
+    private readonly UserDto _model = new();
+    private IValidator<UserDto> _validator = default!;
+
+    protected override void OnInitialized()
+    {
+        _validator = UserValidatorFactory.CreateValidator();
+    }
+
+    private void OnPasswordChanges(string? password)
+    {
+        _model.Password = password ?? "";
+        _model.PasswordConfirm = _model.Password;
+    }
+
+    private async Task Save()
+    {
+        await UserRolesRequests.CreateAsync(_model);
+        NavigationManager.NavigateTo("/users");
+    }
+}
