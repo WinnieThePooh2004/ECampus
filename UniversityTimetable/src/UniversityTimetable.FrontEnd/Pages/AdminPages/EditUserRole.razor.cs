99647e913c0045b39a21c5cf7cf4fbf13dd40672
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-01-20 21:21:33 +0200

    moved edit of user role to a separate component

--- UniversityTimetable/src/UniversityTimetable.FrontEnd/Pages/AdminPages/EditUserRole.razor.cs
+++ UniversityTimetable/src/UniversityTimetable.FrontEnd/Pages/AdminPages/EditUserRole.razor.cs
@@ -3,10 +3,4 @@
 public sealed partial class EditUserRole
 {
     protected override string PageAfterSave => "/users";
-
-    private void RoleChanged(UserRole role)
-    {
-        Model!.Role = role;
-        StateHasChanged();
-    }
 }
