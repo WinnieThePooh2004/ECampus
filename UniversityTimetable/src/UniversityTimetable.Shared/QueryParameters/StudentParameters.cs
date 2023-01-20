538876b9aa3d1174a3cec75b9043b4c58f20c8b2
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-01-20 23:38:54 +0200

    added UserIdCanBeNull property to students and teachers query parameters

--- UniversityTimetable/src/UniversityTimetable.Shared/QueryParameters/StudentParameters.cs
+++ UniversityTimetable/src/UniversityTimetable.Shared/QueryParameters/StudentParameters.cs
@@ -5,7 +5,8 @@ namespace UniversityTimetable.Shared.QueryParameters;
 public class StudentParameters : QueryParameters, IQueryParameters<Student>
 {
     public string? LastName { get; set; }
-    
     public string? FirstName { get; set; }
     public int GroupId { get; set; }
+
+    public bool UserIdCanBeNull { get; set; } = true;
 }
--- UniversityTimetable/src/UniversityTimetable.Shared/QueryParameters/TeacherParameters.cs
+++ UniversityTimetable/src/UniversityTimetable.Shared/QueryParameters/TeacherParameters.cs
@@ -7,4 +7,6 @@ public class TeacherParameters : QueryParameters, IQueryParameters<Teacher>
     public int DepartmentId { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
+
+    public bool UserIdCanBeNull { get; set; } = true;
 }
