58f0c9e27fb601203b09b9dd75a1287ff910164b
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-01-21 00:34:21 +0200

    now do not throw exception when trying to create user with related teacher or student

--- UniversityTimetable/src/UniversityTimetable.Infrastructure/DataAccessFacades/UserRolesDataAccessFacade.cs
+++ UniversityTimetable/src/UniversityTimetable.Infrastructure/DataAccessFacades/UserRolesDataAccessFacade.cs
@@ -29,7 +29,32 @@ public class UserRolesDataAccessFacade : IUserRolesDataAccessFacade
 
     public async Task<User> CreateAsync(User user)
     {
+        var teacher = user.Teacher;
+        var student = user.Student;
+        user.Teacher = null;
+        user.Student = null;
         _context.Add(user);
+        await _context.SaveChangesAsync();
+        return user.Role switch
+        {
+            UserRole.Teacher when teacher is not null => await SetTeacherId(user, teacher),
+            UserRole.Student when student is not null => await SetStudentId(user, student),
+            _ => user
+        };
+    }
+
+    private async Task<User> SetStudentId(User user, Student student)
+    {
+        student.UserId = user.Id;
+        _context.Update(student);
+        await _context.SaveChangesAsync();
+        return user;
+    }
+
+    private async Task<User> SetTeacherId(User user, Teacher teacher)
+    {
+        teacher.UserId = user.Id;
+        _context.Update(teacher);
         await _context.SaveChangesAsync();
         return user;
     }
