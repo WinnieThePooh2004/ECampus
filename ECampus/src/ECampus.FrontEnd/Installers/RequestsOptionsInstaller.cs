7a25154fe55c02c271761f212470b74631bdba73
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-01-23 19:07:28 +0200

    added RequestsOptionsInstaller

---
+++ ECampus/src/ECampus.FrontEnd/Installers/RequestsOptionsInstaller.cs
@@ -0,0 +1,13 @@
+﻿using ECampus.FrontEnd.Requests.Options;
+using ECampus.Shared.Installers;
+
+namespace ECampus.FrontEnd.Installers;
+
+public class RequestsOptionsInstaller : IInstaller
+{
+    public int InstallOrder => -1;
+    public void Install(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<IRequestOptions>(new RequestOptions(configuration));
+    }
+}
