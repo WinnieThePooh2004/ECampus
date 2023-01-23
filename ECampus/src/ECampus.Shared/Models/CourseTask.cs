2b9522074df7cc05d4bfd9011d5eff2fea68f0b1
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-01-23 21:44:37 +0200

    added name property to CourseTask, CourseTaskDto and CourseTasks table

--- ECampus/src/ECampus.Shared/Models/CourseTask.cs
+++ ECampus/src/ECampus.Shared/Models/CourseTask.cs
@@ -6,6 +6,7 @@ namespace ECampus.Shared.Models;
 public class CourseTask : IModel, IIsDeleted
 {
     public int Id { get; set; }
+    public string Name { get; set; } = default!;
     public bool IsDeleted { get; set; }
     public DateTime Deadline { get; set; }
     public bool ValidAfterDeadline { get; set; }
