ddf620dc90cc9cb5ffac111f1e1ba2cd2bdacd1f
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-01-24 23:22:30 +0200

    now send message when new task created

---
+++ ECampus/src/ECampus.Shared/Interfaces/DataAccess/ICourseTaskMessageDataAccess.cs
@@ -0,0 +1,6 @@
+﻿namespace ECampus.Shared.Interfaces.DataAccess;
+
+public interface ICourseTaskMessageDataAccess
+{
+    Task<(string courseName, List<string> studentEmails)> LoadDataForSendMessage(int courseId);
+}
