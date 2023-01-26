d0330d1183346c85b2494d3610d34c93a8267cde
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-01-27 00:21:00 +0200

    added IsMarked column to TaskSubmissions table

---
+++ ECampus/Migrations/Migrations/20230126221802_AddedIsMarkedToTaskSubmissions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedIsMarkedToTaskSubmissions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMarked",
+                table: "TaskSubmissions",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsMarked",
+                table: "TaskSubmissions");
+        }
+    }
+}
--- ECampus/src/ECampus.Shared/DataTransferObjects/TaskSubmissionDto.cs
+++ ECampus/src/ECampus.Shared/DataTransferObjects/TaskSubmissionDto.cs
@@ -10,12 +10,12 @@ public class TaskSubmissionDto : IDataTransferObject
     public int Id { get; set; }
     public int CourseTaskId { get; set; }
     public int StudentId { get; set; }
-    public string SubmissionContent { get; set; } = string.Empty;
+    public bool IsMarked { get; set; }
+    [NotDisplay] public string SubmissionContent { get; set; } = string.Empty;
     
-    [DisplayName("Total points")]
-    public int TotalPoints { get; set; }
+    [DisplayName("Total points", int.MaxValue - 1)] public int TotalPoints { get; set; }
     
     public CourseTaskDto? CourseTask { get; set; }
 
-    public StudentDto Student { get; set; } = default!;
+    public StudentDto? Student { get; set; }
 }
