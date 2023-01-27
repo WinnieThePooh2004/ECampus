1c72c01278d2ddff07a43b4970418e96e64eafdb
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-01-27 15:20:08 +0200

    now sends email when submission is edited or marked

---
+++ ECampus/src/ECampus.Messaging/MessageHandlers/TaskSubmissions/SubmissionEditedHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Mail;
+using ECampus.Messaging.Mailing;
+using ECampus.Messaging.Messages;
+using MediatR;
+using ILogger = Serilog.ILogger;
+
+namespace ECampus.Messaging.MessageHandlers.TaskSubmissions;
+
+public class SubmissionEditedHandler : IRequestHandler<SubmissionEdited>
+{
+    private readonly IEmailSendService _emailSendService;
+    private readonly ILogger _logger;
+
+    public SubmissionEditedHandler(IEmailSendService emailSendService, ILogger logger)
+    {
+        _emailSendService = emailSendService;
+        _logger = logger;
+    }
+
+    public async Task<Unit> Handle(SubmissionEdited request, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(request.UserEmail))
+        {
+            _logger.Warning("User email is null or empty, cannot send email");
+            return Unit.Value;
+        }
+
+        Log(request);
+        var email = new MailMessage
+        {
+            Body = $"<h2>Your submission on task {request.TaskName} was edited</h2>",
+            Subject = "Submission was edited",
+            IsBodyHtml = true
+        };
+        await _emailSendService.SendEmailAsync(email, new List<string> { request.UserEmail });
+        return Unit.Value;
+    }
+
+    private void Log(SubmissionEdited request)
+    {
+        _logger.Information("Submission was edited:\n" +
+                            "Student`s email: {Email}\n" +
+                            "Task name: {Name}", request.UserEmail, request.TaskName);
+    }
+}
