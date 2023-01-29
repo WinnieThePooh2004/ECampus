7c74c5fc6cbe5b5626536716e1e086d905cc63c5
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-01-30 00:33:25 +0200

    how have constructor

--- ECampus/tests/ECampus.Tests.Shared/Mocks/Options.cs
+++ ECampus/tests/ECampus.Tests.Shared/Mocks/Options.cs
@@ -4,5 +4,10 @@ namespace ECampus.Tests.Shared.Mocks;
 
 public class Options<T> : IOptions<T> where T : class
 {
-    public required T Value { get; init; }
+    public T Value { get; }
+
+    public Options(T value)
+    {
+        Value = value;
+    }
 }
