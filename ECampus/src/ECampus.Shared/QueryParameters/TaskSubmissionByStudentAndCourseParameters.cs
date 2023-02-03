f9dbee62d5c8283bfcb9917aee1598fd696297a0
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-02-03 23:23:12 +0200

    IRelationsDataAccess.cs is no longer generic and is renamed to, IDataAccessManager now throws exception when not found object by id

---
+++ ECampus/src/ECampus.Shared/QueryParameters/TaskSubmissionByStudentAndCourseParameters.cs
@@ -0,0 +1,9 @@
+﻿using ECampus.Shared.Models;
+
+namespace ECampus.Shared.QueryParameters;
+
+public class TaskSubmissionByStudentAndCourseParameters : IQueryParameters<TaskSubmission>
+{
+    public int StudentId { get; set; }
+    public int CourseTaskId { get; set; }
+}
