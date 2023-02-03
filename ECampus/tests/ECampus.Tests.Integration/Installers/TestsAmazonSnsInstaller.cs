237a9a23a1f07f75859d0732306475e0d132326b
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-02-04 01:02:20 +0200

    now exist not more than one AmazonSnsMock

--- ECampus/tests/ECampus.Tests.Integration/Installers/TestsAmazonSnsInstaller.cs
+++ ECampus/tests/ECampus.Tests.Integration/Installers/TestsAmazonSnsInstaller.cs
@@ -12,13 +12,28 @@ public class TestsAmazonSnsInstaller : IInstaller
 {
     public int InstallOrder => -1;
 
+    private static IAmazonSimpleNotificationService AmazonSnsMock
+    {
+        get
+        {
+            if (_amazonSnsMock is not null)
+            {
+                return _amazonSnsMock;
+            }
+            
+            _amazonSnsMock = Substitute.For<IAmazonSimpleNotificationService>();
+            _amazonSnsMock.FindTopicAsync(Arg.Any<string>()).Returns(new Topic { TopicArn = "IntegrationTests" });
+            ApplicationFactory.AmazonSnsMock = _amazonSnsMock;
+            return _amazonSnsMock;
+        }
+    }
+
+    private static IAmazonSimpleNotificationService? _amazonSnsMock;
+
     public void Install(IServiceCollection services, IConfiguration configuration)
     {
         var descriptor = services.Single(service => service.ServiceType == typeof(IAmazonSimpleNotificationService));
         services.Remove(descriptor);
-        var amazonSns = Substitute.For<IAmazonSimpleNotificationService>();
-        amazonSns.FindTopicAsync(Arg.Any<string>()).Returns(new Topic { TopicArn = "IntegrationTests" });
-        ApplicationFactory.AmazonSnsMock = amazonSns;
-        services.AddSingleton(amazonSns);
+        services.AddSingleton(AmazonSnsMock);
     }
 }
