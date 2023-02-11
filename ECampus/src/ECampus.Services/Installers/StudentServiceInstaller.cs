c67d20457b3c9446cc92c08debc6587dfe8e784e
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-02-11 15:10:13 +0200

    moved create task submission when student is created to service

--- ECampus/src/ECampus.Services/Installers/StudentServiceInstaller.cs
+++ ECampus/src/ECampus.Services/Installers/StudentServiceInstaller.cs
@@ -1,18 +1,17 @@
 ﻿using ECampus.Contracts.Services;
 using ECampus.Core.Installers;
 using ECampus.Services.Services;
-using ECampus.Services.Services.ValidationServices;
+using ECampus.Shared.DataTransferObjects;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ECampus.Services.Installers;
 
-public class TaskSubmissionInstaller : IInstaller
+public class StudentServiceInstaller : IInstaller
 {
-    public int InstallOrder => -1;
+    public int InstallOrder => 0;
     public void Install(IServiceCollection services, IConfiguration configuration)
     {
-        services.AddScoped<ITaskSubmissionService, TaskSubmissionService>();
-        services.Decorate<ITaskSubmissionService, TaskSubmissionValidationService>();
+        services.Decorate<IBaseService<StudentDto>, StudentService>();
     }
 }
