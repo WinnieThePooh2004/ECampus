5fc11432a5e8f19d6d3cc2e6dd7e18fe2ca586d2
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-02-13 23:56:04 +0200

    added validation for new type

---
+++ ECampus/src/ECampus.Domain/Mapping/TeacherRateProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ECampus.Shared.DataTransferObjects;
+using ECampus.Shared.Models;
+
+namespace ECampus.Domain.Mapping;
+
+public class TeacherRateProfile : Profile
+{
+    public TeacherRateProfile()
+    {
+        CreateMap<TeacherRate, TeacherRateDto>().ForMember(
+            dest => dest.CourseName,
+            opt =>
+                opt.MapFrom(c => c.Course!.Name)
+        ).ForMember(
+            dest => dest.TeachersName,
+            opt =>
+                opt.MapFrom(c => c.Teacher!.FirstName + c.Teacher.LastName)
+        );
+    }
+}
