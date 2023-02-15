7bd1f0653232fc8f810845d7a64c2e69a81193ac
WinnieThePooh2004 <vova2004hunko@icloud.com>
2023-02-15 14:23:47 +0200

    removed migrations project from repository

--- ECampus/Migrations/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Migrations;
-
-var dbBuilder = new ApplicationContextFactory();
-using var context = dbBuilder.CreateDbContext(args);
-context.Database.Migrate();
